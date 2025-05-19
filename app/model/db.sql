4366ea9cacae8794997afdbe6b5dbaedb8125a96
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-05-19 11:59:07 -0400

    Atualizações Db

--- app/model/db.sql
+++ app/model/db.sql
@@ -1,14 +1,29 @@
-CREATE DATABASE dengue;
+REATE DATABASE dengue;
 USE dengue;
-SHOW TABLES;
 
-CREATE TABLE administrador(
-    id_administrador INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(250),
-    email VARCHAR(200),
-    senha VARCHAR(250),
-    PRIMARY KEY(id_administrador)
+
+CREATE TABLE perfil (
+	id_perfil INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(150),
+    PRIMARY KEY(id_perfil)
 );
+
+INSERT INTO perfil (nome) VALUES ('Administrador');
+INSERT INTO perfil (nome) VALUES ('Supervisor');
+
+CREATE TABLE usuario (
+    id_usuario INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(250),
+    email VARCHAR(200) NOT NULL UNIQUE,
+    senha VARCHAR(255),
+    id_perfil INT NOT NULL,
+    PRIMARY KEY(id_usuario),
+    FOREIGN KEY(id_perfil) REFERENCES perfil(id_perfil)
+);
+
+INSERT INTO usuario (nome, email, senha, id_perfil) VALUES ('administrador', 'administrador@outlook.com', '123', '1');
+INSERT INTO usuario (nome, email, senha, id_perfil) VALUES ('supervisor', 'supervisor@outlook.com', '123', '2');
+SELECT * FROM usuario;
 
 CREATE TABLE estado(
 	id_estado INT NOT NULL AUTO_INCREMENT,
@@ -19,7 +34,7 @@ CREATE TABLE estado(
 CREATE TABLE cidade(
     id_cidade INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(250),
-    id_estado INT,
+    id_estado INT NOT NULL,
     PRIMARY KEY(id_cidade),
     FOREIGN KEY(id_estado) REFERENCES estado(id_estado)
 );
@@ -27,7 +42,7 @@ CREATE TABLE cidade(
 CREATE TABLE escola(
     id_escola INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(250),
-    id_cidade INT,
+    id_cidade INT NOT NULL,
     ativo INT DEFAULT 0,
     PRIMARY KEY(id_escola),
     FOREIGN KEY(id_cidade) REFERENCES cidade(id_cidade)
@@ -36,7 +51,7 @@ CREATE TABLE escola(
 CREATE TABLE serie(
     id_serie INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100),
-    id_escola INT,
+    id_escola INT NOT NULL,
     PRIMARY KEY(id_serie),
     FOREIGN KEY(id_escola) REFERENCES escola(id_escola)
 );
@@ -44,7 +59,7 @@ CREATE TABLE serie(
 CREATE TABLE aluno(
     id_aluno INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(250),
-    id_serie INT,
+    id_serie INT NOT NULL,
     PRIMARY KEY(id_aluno),
     FOREIGN KEY(id_serie) REFERENCES serie(id_serie)
 );
@@ -133,88 +148,3 @@ BEGIN
 END$$
 
 DELIMITER ;
-
-
-
-
-
-
--- insert --
-INSERT INTO estado (nome) VALUES ('Mato Grosso do Sul');
-INSERT INTO cidade (nome, id_estado) VALUES ('Campo Grande', 1);
-INSERT INTO escola (nome, id_cidade, ativo) VALUES ('Escola Estadual Adventor Divino de Almeida', 1, 1);
-INSERT INTO serie (nome, id_escola) VALUES ('8º Ano B', 1);
-INSERT INTO aluno (nome, id_serie) VALUES ('Guilherme Machado', 1);
-INSERT INTO pontos_aluno (id_aluno, pontos) VALUES (1, 3000);
-
-
--- consultas --
-
-SELECT * FROM aluno;
-SELECT * FROM serie;
-SELECT * FROM pontos_aluno;
-SELECT * FROM pontos_escola;
-
-
-UPDATE pontos_aluno
-SET pontos = pontos + 6000
-WHERE id_aluno = 1;
-
-
-
-
-
-
--- Consultar todos os estados e suas respectivas cidades
-SELECT estado.nome AS Estado, GROUP_CONCAT(cidade.nome ORDER BY cidade.nome) AS Cidades
-FROM estado
-JOIN cidade ON estado.id_estado = cidade.id_estado
-GROUP BY estado.id_estado;
-
--- Consultar todas as escolas em uma cidade específica
-SELECT escola.nome AS Escola, cidade.nome AS Cidade, estado.nome AS Estado
-FROM escola
-JOIN cidade ON escola.id_cidade = cidade.id_cidade
-JOIN estado ON cidade.id_estado = estado.id_estado
-WHERE cidade.nome = 'Campo Grande';
-
--- Consultar a soma de pontos de todos os alunos de uma escola
-SELECT escola.nome AS Escola, SUM(pontos_aluno.pontos) AS Total_Pontos
-FROM pontos_aluno
-JOIN aluno ON pontos_aluno.id_aluno = aluno.id_aluno
-JOIN serie ON aluno.id_serie = serie.id_serie
-JOIN escola ON serie.id_escola = escola.id_escola
-GROUP BY escola.id_escola;
-
--- Consultar a soma de pontos de todos os alunos de todas as escolas
-SELECT escola.nome AS Escola, SUM(pontos_aluno.pontos) AS Total_Pontos
-FROM pontos_aluno
-JOIN aluno ON pontos_aluno.id_aluno = aluno.id_aluno
-JOIN serie ON aluno.id_serie = serie.id_serie
-JOIN escola ON serie.id_escola = escola.id_escola
-GROUP BY escola.id_escola;
-
--- Consultar o total de pontos de uma escola específica (usando a tabela pontos_escola)
-SELECT escola.nome AS Escola, pontos_escola.total_pontos AS Total_Pontos
-FROM pontos_escola
-JOIN escola ON pontos_escola.id_escola = escola.id_escola
-WHERE escola.nome = 'Escola Estadual Maria';
-
--- Consultar os pontos de um aluno específico
-SELECT aluno.nome AS Aluno, pontos_aluno.pontos AS Pontos
-FROM pontos_aluno
-JOIN aluno ON pontos_aluno.id_aluno = aluno.id_aluno
-WHERE aluno.nome = 'Guilherme Machado';
-
--- Consultar o total de pontos das escolas com status ativo (status = 1)
-SELECT escola.nome AS Escola, pontos_escola.total_pontos AS Total_Pontos
-FROM pontos_escola
-JOIN escola ON pontos_escola.id_escola = escola.id_escola
-WHERE escola.ativo = 1;
-
--- Consultar todos os alunos e os pontos de cada um
-SELECT aluno.nome AS Aluno, pontos_aluno.pontos AS Pontos
-FROM aluno
-JOIN pontos_aluno ON aluno.id_aluno = pontos_aluno.id_aluno;
-
-
